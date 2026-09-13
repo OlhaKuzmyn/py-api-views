4318ad98612b126c53a4b6fa8e015cf8a49af09f
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-13 11:35:50 +0300

    added Genre views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import movie_list, movie_detail, GenreListView, GenreDetailView
 
 urlpatterns = [
+    path("genres/", GenreListView.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetailView.as_view(), name="genre-detail"),
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -3,9 +3,46 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
-from cinema.models import Movie
-from cinema.serializers import MovieSerializer
+from cinema.models import Movie, Genre
+from cinema.serializers import MovieSerializer, GenreSerializer
+
+
+class GenreListView(APIView):
+    def get(self, request):
+        genre = Genre.objects.all()
+        serializer = GenreSerializer(genre, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    def post(self, request):
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GenreDetailView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Genre, pk=pk)
+
+    def get(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        genre = self.get_object(pk)
+        genre.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(["GET", "POST"])
