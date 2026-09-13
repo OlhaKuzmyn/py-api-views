a95a9c65ac9535adb7b29f5e973f31ebaf89cb30
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-13 11:00:52 +0300

    fixed typo in field seats_in_a_row added serializers

--- cinema/models.py
+++ cinema/models.py
@@ -13,7 +13,7 @@ class Genre(models.Model):
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
     rows = models.IntegerField()
-    seats_in_rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
 
 
 class Movie(models.Model):
