4f0a2ba1241841860cb1dec65b2ac7a7db9b80c4
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-13 14:00:40 +0300

    added CinemaHall views

--- cinema/urls.py
+++ cinema/urls.py
@@ -6,7 +6,24 @@ from cinema.views import (
     GenreListView,
     GenreDetailView,
     ActorListView,
-    ActorDetailView
+    ActorDetailView, CinemaHallViewSet
+)
+
+
+cinema_hall_list = CinemaHallViewSet.as_view(
+    {
+        "get": "list",
+        "post": "create",
+    }
+)
+
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    {
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
 )
 
 urlpatterns = [
@@ -14,6 +31,8 @@ urlpatterns = [
     path("genres/<int:pk>/", GenreDetailView.as_view(), name="genre-detail"),
     path("actors/", ActorListView.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
+    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema-halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
